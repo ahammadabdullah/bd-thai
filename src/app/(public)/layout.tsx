e7680158f92e0159f96b5a787000c5433873646c
Ahammad Abdullah <ahammadabdullahnew@gmail.com>
2025-03-13 20:06:49 +0600

    feat(cms) cms ui added

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,21 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import { NextIntlClientProvider } from "next-intl";
-import { getLocale, getMessages } from "next-intl/server";
+import { getMessages } from "next-intl/server";
 import { Navbar } from "@/components/Navbar/navbar";
 import InquiryForm from "@/components/form/inquiry-form";
 import { Footer } from "@/components/footer/footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "BD Thai Food",
