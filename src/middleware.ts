82d6afc28fbc6f8d1755fdc93dee43833af79371
Ahammad Abdullah <ahammadabdullahnew@gmail.com>
2025-03-25 00:18:08 +0600

    feat(middleware) private routes added

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import NextAuth, { NextAuthConfig } from "next-auth";
+import { NextResponse, NextRequest } from "next/server";
+import { authConfig } from "./auth/auth.config";
+
+const { auth } = NextAuth(authConfig as NextAuthConfig);
+
+export async function middleware(request: NextRequest) {
+  const { nextUrl } = request;
+  const { pathname } = nextUrl;
+  const session = await auth();
+  const isLoggedIn = !!session?.user;
+  const privateRoutes = [
+    "/dashboard",
+    "/dashboard/blogs",
+    "/dashboard/users",
+    "/dashboard/quotations",
+  ];
+
+  if (isLoggedIn && pathname === "/login") {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  if (!isLoggedIn && privateRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/((?!api|api/auth|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
